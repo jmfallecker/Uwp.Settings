fb7e54a49d50cf5c38882505a79b300654c2be50
Joe Fallecker <jmfallecker@gmail.com>
2018-11-15 21:58:19 -0500

    Added TryRead, TryWrite and SafeRead functions to Settings.cs.
    These functions will give more flexibility, also allows users of this to not have to try/catch everything.

--- Uwp.Settings/Settings.cs
+++ Uwp.Settings/Settings.cs
@@ -31,7 +31,7 @@ namespace Uwp.Settings
         /// </summary>
         /// <typeparam name="T">The expected type of the setting.</typeparam>
         /// <param name="settingName">The name of the setting.</param>
-        /// <param name="dataStore">The type of the setting. e.g. Local, Roaming, etc.</param> 
+        /// <param name="dataStore">The storage to read from. e.g. Local, Roaming, etc.</param> 
         /// <exception cref="ArgumentException"></exception>
         /// <exception cref="InvalidCastException"></exception>
         /// <returns>Value of the setting.</returns>
@@ -42,12 +42,97 @@ namespace Uwp.Settings
         }
 
         /// <summary>
+        /// Attempts to read the value for given <paramref name="settingName"/>
+        /// </summary>
+        /// <typeparam name="T">The expected type of the setting.</typeparam>
+        /// <param name="settingName">The name of the setting.</param>
+        /// <param name="settingValue">Will be default(<typeparamref name="T"/>) if no value found.</param>
+        /// <returns>Value of the setting.</returns>
+        public static bool TryRead<T>(string settingName, out T settingValue)
+        {
+            try
+            {
+                settingValue = _settingsService.Read<T>(settingName);
+                return true;
+            }
+            catch (Exception)
+            {
+                settingValue = default(T);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to read the value from the given <paramref name="dataStore"/> for given <paramref name="settingName"/>
+        /// </summary>
+        /// <typeparam name="T">The expected type of the setting.</typeparam>
+        /// <param name="settingName">The name of the setting.</param>
+        /// <param name="settingValue">Will be default(<typeparamref name="T"/>) if no value found.</param>
+        /// <param name="dataStore">The storage to read from. e.g. Local, Roaming, etc.</param> 
+        /// <returns>Value of the setting.</returns>
+        public static bool TryRead<T>(string settingName, out T settingValue, DataStore dataStore)
+        {
+            try
+            {
+                var service = new SettingsService(dataStore);
+
+                settingValue = service.Read<T>(settingName);
+                return true;
+            }
+            catch (Exception)
+            {
+                settingValue = default(T);
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Returns <paramref name="fallbackValue"/> if setting value is not found.
+        /// </summary>
+        /// <typeparam name="T">The expected type of the setting.</typeparam>
+        /// <param name="settingName">The name of the setting.</param>
+        /// <param name="fallbackValue">Value to return if no setting value is found.</param>
+        /// <returns></returns>
+        public static T SafeRead<T>(string settingName, T fallbackValue)
+        {
+            if (TryRead(settingName, out T settingValue))
+            {
+                return settingValue;
+            }
+            else
+            {
+                return fallbackValue;
+            }
+        }
+
+        /// <summary>
+        /// Returns <paramref name="fallbackValue"/> if setting value is not found.
+        /// </summary>
+        /// <typeparam name="T">The expected type of the setting.</typeparam>
+        /// <param name="settingName">The name of the setting.</param>
+        /// <param name="fallbackValue">Value to return if no setting value is found.</param>
+        /// <param name="dataStore">The storage to read from. e.g. Local, Roaming, etc.</param>
+        /// <returns></returns>
+        public static T SafeRead<T>(string settingName, T fallbackValue, DataStore dataStore)
+        {
+            if (TryRead(settingName, out T settingValue, dataStore))
+            {
+                return settingValue;
+            }
+            else
+            {
+                return fallbackValue;
+            }
+        }
+
+        /// <summary>
         /// Write the setting to Local Settings.
         /// </summary>
+        /// <typeparam name="T">The type of the setting's value.</typeparam>
         /// <param name="settingName">The name of the setting.</param>
         /// <param name="settingValue">The value of the setting.</param>
         /// <returns>True if written, false if write failed.</returns>
-        public static void Write(string settingName, object settingValue)
+        public static void Write<T>(string settingName, T settingValue)
         {
             _settingsService.Write(settingName, settingValue);
         }
@@ -55,14 +140,56 @@ namespace Uwp.Settings
         /// <summary>
         /// Write the setting to Local Settings.
         /// </summary>
+        /// <typeparam name="T">The type of the setting's value.</typeparam>
         /// <param name="settingName">The name of the setting.</param>
         /// <param name="settingValue">The value of the setting.</param>
-        /// <param name="dataStore">The type of the setting. e.g. Local, Roaming, etc.</param>
+        /// <param name="dataStore">The storage to write to. e.g. Local, Roaming, etc.</param>
         /// <returns>True if written, false if write failed.</returns>
-        public static void Write(string settingName, object settingValue, DataStore dataStore)
+        public static void Write<T>(string settingName, T settingValue, DataStore dataStore)
         {
             ISettingsService service = new SettingsService(dataStore);
             service.Write(settingName, settingValue);
+        }
+
+        /// <summary>
+        /// Attempts to write the setting and value.
+        /// </summary>
+        /// <typeparam name="T">The type of the setting's value.</typeparam>
+        /// <param name="settingName">The name of the setting.</param>
+        /// <param name="settingValue">The value of the setting.</param>
+        /// <returns>True if setting is written, false otherwise.</returns>
+        public static bool TryWrite<T>(string settingName, T settingValue)
+        {
+            try
+            {
+                Write(settingName, settingValue);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to write the setting and value.
+        /// </summary>
+        /// <typeparam name="T">The type of the setting's value.</typeparam>
+        /// <param name="settingName">The name of the setting.</param>
+        /// <param name="settingValue">The value of the setting.</param>
+        /// <param name="dataStore">The storage to write to. e.g. Local, Roaming, etc.</param>
+        /// <returns>True if setting is written, false otherwise.</returns>
+        public static bool TryWrite<T>(string settingName, T settingValue, DataStore dataStore)
+        {
+            try
+            {
+                Write(settingName, settingValue, dataStore);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         /// <summary>
